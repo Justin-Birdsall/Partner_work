235b6ab3e973191ef76c1e8d2dcb5266b7dcd0fc
Richard Roy <richard.alex.roy@gmail.com>
2023-12-01 09:23:10 -0500

    😵‍💫

--- HW6/HW6.1/wackySumTheSequal.asm
+++ HW6/HW6.1/wackySumTheSequal.asm
@@ -2,10 +2,11 @@
 
 
 combineFour:
-    addi $sp, $sp, -12
-    sw $s0, 0($sp)
-    sw $s1, 4($sp)
-    sw $s2, 8($sp)
+    addi $sp, $sp, -16
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    sw $s2, 12($sp)
 
     add $t0, $a0, $a1
     add $t0, $t0, $a2
@@ -18,10 +19,18 @@ combineFour:
 
 even:
     addi $v0, $t0, 0
-    jr $ra
+    j endcombine
 
 odd:
     #divide sum by 2
     srl $t0, $t0, 1
     addi $v0, $t0, 0
+    j endcombine
+
+endcombine:
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    lw $s2, 12($sp)
+    addi $sp, $sp, 16
     jr $ra
