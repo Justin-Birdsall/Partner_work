d5ed946f8116828a7c048f1d664d3c0d18256aac
Richard Roy <richard.alex.roy@gmail.com>
2023-12-01 09:51:59 -0500

    ()()

--- HW6/HW6.1/wackySumTheSequal.asm
+++ HW6/HW6.1/wackySumTheSequal.asm
@@ -1,5 +1,59 @@
 
 
+addi $a0, $0, 3
+addi $a1, $0, 7
+addi $a2, $0, 2
+j wackySum
+syscall
+
+wackySum:
+    addi $sp, $sp, 28
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    sw $s2, 12($sp)
+
+    #backup $a0-$a2 in s regs
+    move $s0, $a0
+    move $s1, $a1
+    move $s2, $a2
+
+    move $t0, $a0 #t is incrementor
+    addi $t1, $0, 0 #t1 is sum
+startloop:
+    bgt $t0, $s1, endloop
+
+    addi $a0, $t0, 0
+    addi $a1, $t0, 1
+    srl $a1, $a1, 1
+    addi $a2, $t0, 2
+    srl $a2, $a2, 1
+    addi $a3, $t0, 3
+
+    sw $t0, 16($sp)
+    sw $t1, 20($sp)
+    sw $t2, 24($sp)
+
+    jal combineFour
+
+    lw $t2, 24($sp)
+    lw $t1, 20($sp)
+    lw $t0, 16($sp)
+
+    add $t1, $t1, $v0
+    add $t0, $t0, $s2
+    addi $v0, $t1, 0
+    j startloop
+
+endloop:
+    move $v0, $t1
+
+    lw $s2, 12($sp)
+    lw $s1, 8($sp)
+    lw $s0, 4($sp)
+    lw $ra, 0($sp)
+    addi $sp, $sp, 28
+    jr $ra
 
 combineFour:
     addi $sp, $sp, -16
@@ -28,9 +82,9 @@ odd:
     j endcombine
 
 endcombine:
-    lw $ra, 0($sp)
-    lw $s0, 4($sp)
-    lw $s1, 8($sp)
     lw $s2, 12($sp)
+    lw $s1, 8($sp)
+    lw $s0, 4($sp)
+    lw $ra, 0($sp)
     addi $sp, $sp, 16
     jr $ra
