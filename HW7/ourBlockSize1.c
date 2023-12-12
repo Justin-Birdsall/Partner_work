b47a277fb2ef2818d36d266680128572af6073f0
Justin Birdsall <Birdsaju@mail.gvsu.edu>
2023-12-12 17:04:06 -0500

    OHahahhaha

---
+++ HW7/ourBlockSize1.c
@@ -0,0 +1,45 @@
+/* i.e., 64KB.  The array needs to be at least twice the size of the
+   8KB cache so that the array doesn't fit in memory. */
+#define ARRAY_SIZE 64*1024
+
+#define NUM_LOOPS 10000000
+
+
+/****************************************************************************
+ *
+ *  Notice that the array is an array of characters.  This means that
+ *  each item in the cache is exactly 1 byte.  This makes it easy to
+ *  identify data items that will or will not conflict in the cache.
+ *  For example, in an 8KB direct-mapped cache, array bytes 0 and 8192
+ *  will conflict.
+ *
+ *  This program simply iterates through each byte in the cache NUM_LOOPS
+ *  times.
+ *
+ * Declaring the local variables as "register" variables encourages
+ * the compiler to keep the values of these variables in a register,
+ * thereby reducing their effect on the cache hit rate.
+ ****************************************************************************/
+
+int main() {
+  _Alignas(64)  /* make sure that the array aligns with the cache. */
+  char array[ARRAY_SIZE];
+  register int outer_loop;
+  register int inner_loop;
+  register int solution = 0;
+  register int odd;
+  register int even;
+  odd = 0;
+  even = 128;
+  int balls = 0;
+  for (outer_loop = 0; outer_loop < NUM_LOOPS; outer_loop++) {
+  
+        //solution *= array[0];
+      balls *= array[0];
+      //balls -= array[8192];
+        //solution *= array[];
+      
+  }
+
+  return solution;
+}
